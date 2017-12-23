4886878ad96e3f2e4404fb67a4bce06d35cf4116
jinzhanye <jinzhan.ye@foxmail.com>
2017-12-23 23:33:20 +0800

    es6 module

---
+++ ES6/module/index.js
@@ -0,0 +1,11 @@
+// ComponentA中导出多个变量，所以引入的时候名称要跟ComponentA相同
+import {A, test, Hello} from './ComponentA';
+
+//相当于ComponentA{A, test, Hello}
+import * as ComponentA from './ComponentA';
+
+//由于ComponentB使用export default的方式导出，所以导入的时候可以取任意名称
+import HelloB from './ComponentB';
+
+console.log(A,test,Hello);
+console.log(ComponentA.A, ComponentA.test, ComponentA.Hello);
