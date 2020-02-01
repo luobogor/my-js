6d09138752b2db0f7615cb6e5087c4480c17d658
叶同学 <jinzhanye9@gmail.com>
2020-02-01 15:44:25 +0800

    添加 export 用法

--- ES6/module/ComponentA.js
+++ ES6/module/ComponentA.js
@@ -14,6 +14,16 @@ export class Hello {
 // 即 export const caSub2 = xxxxx
 export { default as caSub2Hello1, hello2 as caSub2Hello2 } from './ComponentA-sub2'
 
+let B = 456
+let C = 789
+export {
+  B,
+  C
+}
+// 相当于
+// export const B = 456
+// export const C = 789
+
 // 相当于重定向 default 模块
 // 即 export default xxx
 export { default } from './ComponentA-sub'
