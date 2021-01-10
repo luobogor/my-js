b39a8ee29a10ee19069a50d7af648bc2a47b640f
叶同学 <jinzhanye9@gmail.com>
2021-01-10 14:36:12 +0800

    add writeHead

--- NodeTutorial/5.creat-server/app.js
+++ NodeTutorial/5.creat-server/app.js
@@ -1,9 +1,20 @@
-var http = require('http');
+const http = require('http');
 
-var server = http.createServer(function (req, res) {
-    console.log('request was made:', req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hey git');
+const server = http.createServer(function (req, res) {
+  console.log('request was made:', req.url);
+
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('X-Foo', 'bar');
+
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+
+  // writeHead 优先级更高 header 最终返回
+  // Content-Type: text/plain
+  // X-Foo: bar
+
+  res.end('Hey git');
 });
 
 server.listen(8081, '127.0.0.1');
