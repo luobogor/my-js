752276ac6862c7eec513e5537e017e89a074ea7a
yejinzhan <jinzhan.ye@foxmail.com>
2023-08-31 10:32:13 +0800

    update

--- NodeTutorial/12.async/index.js
+++ NodeTutorial/12.async/index.js
@@ -1,9 +1,11 @@
 setImmediate(() => {
   console.log('setImmediate');
 });
+
 setTimeout(() => {
   console.log('setTimeout');
 }, 0);
+
 process.nextTick(() => {
   console.log('nextTick1');
   process.nextTick(() => {
@@ -11,4 +13,10 @@ process.nextTick(() => {
   });
 });
 
+
+// 打印顺序
+// nextTick1
+// nextTick2
+// setTimeout
+// setImmediate
 // 优先级 nextTick > setTimeout > setImmediate
